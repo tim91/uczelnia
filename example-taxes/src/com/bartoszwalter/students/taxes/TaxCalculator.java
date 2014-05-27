85e796152645ebee4c5d871aa6cb72ea0a93ccc6
kmoszumanska <kmoszumanska@gmail.com>
2014-05-27 11:01:54 +0200

    Pokrycie kodu testammi

--- example-taxes/src/com/bartoszwalter/students/taxes/TaxCalculator.java
+++ example-taxes/src/com/bartoszwalter/students/taxes/TaxCalculator.java
@@ -3,37 +3,57 @@ package com.bartoszwalter.students.taxes;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaxCalculator {
 	
 	public static double podstawa = 0;
 	public static char umowa = ' ';
-	// składki na ubezpieczenia społeczne
+	// skĹ‚adki na ubezpieczenia spoĹ‚eczne
 	public static double s_emerytalna = 0; // 9,76% podstawyy
 	public static double s_rentowa = 0; // 1,5% podstawy
 	public static double u_chorobowe = 0; // 2,45% podstawy
-	// składki na ubezpieczenia zdrowotne
+	// skĹ‚adki na ubezpieczenia zdrowotne
 	public static double kosztyUzyskania = 111.25; 
 	public static double s_zdrow1 = 0; // od podstawy wymiaru 9%
 	public static double s_zdrow2 = 0; // od podstawy wymiaru 7,75 %
 	public static double zaliczkaNaPod = 0; // zaliczka na podatek dochodowy 18%
-	public static double kwotaZmiejsz = 46.33; // kwota zmienjszająca podatek 46,33 PLN
+	public static double kwotaZmiejsz = 46.33; // kwota zmienjszajÄ…ca podatek 46,33 PLN
 	public static double zaliczkaUS = 0;
 	public static double zaliczkaUS0 = 0;
-
+	
+	public static void reset(){
+		podstawa = 0;
+		umowa = ' ';
+		s_emerytalna = 0;
+		s_rentowa = 0;
+		u_chorobowe = 0;
+		kosztyUzyskania = 111.25;
+		s_zdrow1 = 0;
+		s_zdrow2 = 0;
+		zaliczkaNaPod = 0;
+		kwotaZmiejsz = 46.33;
+		zaliczkaUS = 0;
+		zaliczkaUS0 = 0;
+	}
+	
 	public static void main(String[] args) {
+		
+		ArrayList<String> outPut = new ArrayList<String>();
+		
 		try {
 			InputStreamReader isr = new InputStreamReader(System.in);
 			BufferedReader br = new BufferedReader(isr);
 			
-			System.out.print("Podaj kwotę dochodu: ");	
+			System.out.print("Podaj kwotÄ™ dochodu: ");	
 			podstawa = Double.parseDouble(br.readLine());
 			
 			System.out.print("Typ umowy: (P)raca, (Z)lecenie: ");
 			umowa = br.readLine().charAt(0);
 			
 		} catch (Exception ex) {
-			System.out.println("Błędna kwota");
+			outPut.add("BĹ‚Ä™dna kwota");
 			System.err.println(ex);
 			return;
 		}
@@ -41,93 +61,292 @@ public class TaxCalculator {
 		DecimalFormat df00 = new DecimalFormat("#.00");
 		DecimalFormat df = new DecimalFormat("#");
 		
+		
 		if (umowa == 'P') {
-			System.out.println("UMOWA O PRACĘ");
-			System.out.println("Podstawa wymiaru składek " + podstawa);
+			outPut.add("UMOWA O PRACÄ�");
+			outPut.add("Podstawa wymiaru skĹ‚adek " + podstawa);
 			double oPodstawa = obliczonaPodstawa(podstawa);
-			System.out.println("Składka na ubezpieczenie emerytalne "
+			outPut.add("SkĹ‚adka na ubezpieczenie emerytalne "
 					+ df00.format(s_emerytalna));
-			System.out.println("Składka na ubezpieczenie rentowe    "
+			outPut.add("SkĹ‚adka na ubezpieczenie rentowe    "
 					+ df00.format(s_rentowa));
-			System.out.println("Składka na ubezpieczenie chorobowe  "
+			outPut.add("SkĹ‚adka na ubezpieczenie chorobowe  "
 					+ df00.format(u_chorobowe));
-			System.out
-					.println("Podstawa wymiaru składki na ubezpieczenie zdrowotne: "
+			outPut.add("Podstawa wymiaru skĹ‚adki na ubezpieczenie zdrowotne: "
 							+ oPodstawa);
 			obliczUbezpieczenia(oPodstawa);
-			System.out.println("Składka na ubezpieczenie zdrowotne: 9% = "
+			outPut.add("SkĹ‚adka na ubezpieczenie zdrowotne: 9% = "
 					+ df00.format(s_zdrow1) + " 7,75% = " + df00.format(s_zdrow2));
-			System.out.println("Koszty uzyskania przychodu w stałej wysokości "
+			outPut.add( "Koszty uzyskania przychodu w staĹ‚ej wysokoĹ›ci "
 					+ kosztyUzyskania);
 			double podstawaOpodat = oPodstawa - kosztyUzyskania;
 			double podstawaOpodat0 = Double
 					.parseDouble(df.format(podstawaOpodat));
-			System.out.println("Podstawa opodatkowania " + podstawaOpodat
-					+ " zaokrąglona " + df.format(podstawaOpodat0));
+			outPut.add("Podstawa opodatkowania " + podstawaOpodat
+					+ " zaokrÄ…glona " + df.format(podstawaOpodat0));
 			obliczPodatek(podstawaOpodat0);
-			System.out.println("Zaliczka na podatek dochodowy 18 % = "
+			outPut.add("Zaliczka na podatek dochodowy 18 % = "
 					+ zaliczkaNaPod);
-			System.out.println("Kwota wolna od podatku = " + kwotaZmiejsz);
+			outPut.add("Kwota wolna od podatku = " + kwotaZmiejsz);
 			double podatekPotracony = zaliczkaNaPod - kwotaZmiejsz;
-			System.out.println("Podatek potrącony = "
+			outPut.add("Podatek potrÄ…cony = "
 					+ df00.format(podatekPotracony));
 			obliczZaliczke();
 			zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
-			System.out.println("Zaliczka do urzędu skarbowego = "
-					+ df00.format(zaliczkaUS) + " po zaokrągleniu = "
+			outPut.add("Zaliczka do urzÄ™du skarbowego = "
+					+ df00.format(zaliczkaUS) + " po zaokrÄ…gleniu = "
 					+ df.format(zaliczkaUS0));
 			double wynagrodzenie = podstawa
 					- ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
-			System.out.println();
-			System.out
-					.println("Pracownik otrzyma wynagrodzenie netto w wysokości = "
+			outPut.add("\n");
+			outPut.add("Pracownik otrzyma wynagrodzenie netto w wysokoĹ›ci = "
 							+ df00.format(wynagrodzenie));
 		} else if (umowa == 'Z') {
-			System.out.println("UMOWA-ZLECENIE");
-			System.out.println("Podstawa wymiaru składek " + podstawa);
+			outPut.add("UMOWA-ZLECENIE");
+			outPut.add("Podstawa wymiaru skĹ‚adek " + podstawa);
 			double oPodstawa = obliczonaPodstawa(podstawa);
-			System.out.println("Składka na ubezpieczenie emerytalne "
+			outPut.add("SkĹ‚adka na ubezpieczenie emerytalne "
 					+ df00.format(s_emerytalna));
-			System.out.println("Składka na ubezpieczenie rentowe    "
+			outPut.add("SkĹ‚adka na ubezpieczenie rentowe    "
 					+ df00.format(s_rentowa));
-			System.out.println("Składka na ubezpieczenie chorobowe  "
+			outPut.add("SkĹ‚adka na ubezpieczenie chorobowe  "
 					+ df00.format(u_chorobowe));
 			System.out
-					.println("Podstawa wymiaru składki na ubezpieczenie zdrowotne: "
+					.println("Podstawa wymiaru skĹ‚adki na ubezpieczenie zdrowotne: "
 							+ oPodstawa);
 			obliczUbezpieczenia(oPodstawa);
-			System.out.println("Składka na ubezpieczenie zdrowotne: 9% = "
+			outPut.add("SkĹ‚adka na ubezpieczenie zdrowotne: 9% = "
 					+ df00.format(s_zdrow1) + " 7,75% = " + df00.format(s_zdrow2));
 			kwotaZmiejsz = 0;
 			kosztyUzyskania = (oPodstawa * 20) / 100;
-			System.out.println("Koszty uzyskania przychodu (stałe) "
+			outPut.add("Koszty uzyskania przychodu (staĹ‚e) "
 					+ kosztyUzyskania);
 			double podstawaOpodat = oPodstawa - kosztyUzyskania;
 			double podstawaOpodat0 = Double.parseDouble(df.format(podstawaOpodat));
-			System.out.println("Podstawa opodatkowania " + podstawaOpodat
-					+ " zaokrąglona " + df.format(podstawaOpodat0));
+			outPut.add("Podstawa opodatkowania " + podstawaOpodat
+					+ " zaokrÄ…glona " + df.format(podstawaOpodat0));
 			obliczPodatek(podstawaOpodat0);
-			System.out.println("Zaliczka na podatek dochodowy 18 % = "
+			outPut.add("Zaliczka na podatek dochodowy 18 % = "
 					+ zaliczkaNaPod);
 			double podatekPotracony = zaliczkaNaPod;
-			System.out.println("Podatek potrącony = "
+			outPut.add("Podatek potrÄ…cony = "
 					+ df00.format(podatekPotracony));
 			obliczZaliczke();
 			zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
-			System.out.println("Zaliczka do urzędu skarbowego = "
-					+ df00.format(zaliczkaUS) + " po zaokrągleniu = "
+			outPut.add("Zaliczka do urzÄ™du skarbowego = "
+					+ df00.format(zaliczkaUS) + " po zaokrÄ…gleniu = "
 					+ df.format(zaliczkaUS0));
 			double wynagrodzenie = podstawa
 					- ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
-			System.out.println();
+			outPut.add("\n");
 			System.out
-					.println("Pracownik otrzyma wynagrodzenie netto w wysokości = "
+					.println("Pracownik otrzyma wynagrodzenie netto w wysokoĹ›ci = "
 							+ df00.format(wynagrodzenie));
+			
 		} else {
-			System.out.println("Nieznany typ umowy!");
+			outPut.add("Nieznany typ umowy!");
+		}
+		
+		for (String string : outPut) {
+			System.out.println(string);
 		}
 	}
 
+	
+public static List<String> getArrayWithResults(double kwota,char typ){
+		
+	DecimalFormat df00 = new DecimalFormat("#.00");
+	DecimalFormat df = new DecimalFormat("#");
+	
+	podstawa = kwota;
+	umowa = typ;
+	
+	ArrayList<String> outPut = new ArrayList<String>();
+	
+	if (umowa == 'P') {
+		outPut.add("UMOWA O PRACÄ�");
+		outPut.add("Podstawa wymiaru skĹ‚adek " + podstawa);
+		double oPodstawa = obliczonaPodstawa(podstawa);
+		outPut.add("SkĹ‚adka na ubezpieczenie emerytalne "
+				+ df00.format(s_emerytalna));
+		outPut.add("SkĹ‚adka na ubezpieczenie rentowe    "
+				+ df00.format(s_rentowa));
+		outPut.add("SkĹ‚adka na ubezpieczenie chorobowe  "
+				+ df00.format(u_chorobowe));
+		outPut.add("Podstawa wymiaru skĹ‚adki na ubezpieczenie zdrowotne: "
+						+ oPodstawa);
+		obliczUbezpieczenia(oPodstawa);
+		outPut.add("SkĹ‚adka na ubezpieczenie zdrowotne: 9% = "
+				+ df00.format(s_zdrow1) + " 7,75% = " + df00.format(s_zdrow2));
+		outPut.add( "Koszty uzyskania przychodu w staĹ‚ej wysokoĹ›ci "
+				+ kosztyUzyskania);
+		double podstawaOpodat = oPodstawa - kosztyUzyskania;
+		double podstawaOpodat0 = Double
+				.parseDouble(df.format(podstawaOpodat));
+		outPut.add("Podstawa opodatkowania " + podstawaOpodat
+				+ " zaokrÄ…glona " + df.format(podstawaOpodat0));
+		obliczPodatek(podstawaOpodat0);
+		outPut.add("Zaliczka na podatek dochodowy 18 % = "
+				+ zaliczkaNaPod);
+		outPut.add("Kwota wolna od podatku = " + kwotaZmiejsz);
+		double podatekPotracony = zaliczkaNaPod - kwotaZmiejsz;
+		outPut.add("Podatek potrÄ…cony = "
+				+ df00.format(podatekPotracony));
+		obliczZaliczke();
+		zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
+		outPut.add("Zaliczka do urzÄ™du skarbowego = "
+				+ df00.format(zaliczkaUS) + " po zaokrÄ…gleniu = "
+				+ df.format(zaliczkaUS0));
+		double wynagrodzenie = podstawa
+				- ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
+		outPut.add("\n");
+		outPut.add("Pracownik otrzyma wynagrodzenie netto w wysokoĹ›ci = "
+						+ df00.format(wynagrodzenie));
+	} else if (umowa == 'Z') {
+		outPut.add("UMOWA-ZLECENIE");
+		outPut.add("Podstawa wymiaru skĹ‚adek " + podstawa);
+		double oPodstawa = obliczonaPodstawa(podstawa);
+		outPut.add("SkĹ‚adka na ubezpieczenie emerytalne "
+				+ df00.format(s_emerytalna));
+		outPut.add("SkĹ‚adka na ubezpieczenie rentowe    "
+				+ df00.format(s_rentowa));
+		outPut.add("SkĹ‚adka na ubezpieczenie chorobowe  "
+				+ df00.format(u_chorobowe));
+		outPut.add("Podstawa wymiaru skĹ‚adki na ubezpieczenie zdrowotne: "
+						+ oPodstawa);
+		obliczUbezpieczenia(oPodstawa);
+		outPut.add("SkĹ‚adka na ubezpieczenie zdrowotne: 9% = "
+				+ df00.format(s_zdrow1) + " 7,75% = " + df00.format(s_zdrow2));
+		kwotaZmiejsz = 0;
+		kosztyUzyskania = (oPodstawa * 20) / 100;
+		outPut.add("Koszty uzyskania przychodu (staĹ‚e) "
+				+ kosztyUzyskania);
+		double podstawaOpodat = oPodstawa - kosztyUzyskania;
+		double podstawaOpodat0 = Double.parseDouble(df.format(podstawaOpodat));
+		outPut.add("Podstawa opodatkowania " + podstawaOpodat
+				+ " zaokrÄ…glona " + df.format(podstawaOpodat0));
+		obliczPodatek(podstawaOpodat0);
+		outPut.add("Zaliczka na podatek dochodowy 18 % = "
+				+ zaliczkaNaPod);
+		double podatekPotracony = zaliczkaNaPod;
+		outPut.add("Podatek potrÄ…cony = "
+				+ df00.format(podatekPotracony));
+		obliczZaliczke();
+		zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
+		outPut.add("Zaliczka do urzÄ™du skarbowego = "
+				+ df00.format(zaliczkaUS) + " po zaokrÄ…gleniu = "
+				+ df.format(zaliczkaUS0));
+		double wynagrodzenie = podstawa
+				- ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
+		outPut.add("\n");
+		outPut.add("Pracownik otrzyma wynagrodzenie netto w wysokoĹ›ci = "
+						+ df00.format(wynagrodzenie));
+		
+	} else {
+		outPut.add("Nieznany typ umowy!");
+	}
+	reset();
+	return outPut;
+	}
+	
+	
+	public static List<String> getArrayWithResultsUsingOriginalAlgorithm(double kwota,char typ){
+		
+		DecimalFormat df00 = new DecimalFormat("#.00");
+		DecimalFormat df = new DecimalFormat("#");
+		
+		podstawa = kwota;
+		umowa = typ;
+		
+		ArrayList<String> outPut = new ArrayList<String>();
+		
+		if (umowa == 'P') {
+			outPut.add("UMOWA O PRACÄ�");
+			outPut.add("Podstawa wymiaru skĹ‚adek " + podstawa);
+			double oPodstawa = obliczonaPodstawa(podstawa);
+			outPut.add("SkĹ‚adka na ubezpieczenie emerytalne "
+					+ df00.format(s_emerytalna));
+			outPut.add("SkĹ‚adka na ubezpieczenie rentowe    "
+					+ df00.format(s_rentowa));
+			outPut.add("SkĹ‚adka na ubezpieczenie chorobowe  "
+					+ df00.format(u_chorobowe));
+			outPut.add("Podstawa wymiaru skĹ‚adki na ubezpieczenie zdrowotne: "
+							+ oPodstawa);
+			obliczUbezpieczenia(oPodstawa);
+			outPut.add("SkĹ‚adka na ubezpieczenie zdrowotne: 9% = "
+					+ df00.format(s_zdrow1) + " 7,75% = " + df00.format(s_zdrow2));
+			outPut.add( "Koszty uzyskania przychodu w staĹ‚ej wysokoĹ›ci "
+					+ kosztyUzyskania);
+			double podstawaOpodat = oPodstawa - kosztyUzyskania;
+			double podstawaOpodat0 = Double
+					.parseDouble(df.format(podstawaOpodat));
+			outPut.add("Podstawa opodatkowania " + podstawaOpodat
+					+ " zaokrÄ…glona " + df.format(podstawaOpodat0));
+			obliczPodatek(podstawaOpodat0);
+			outPut.add("Zaliczka na podatek dochodowy 18 % = "
+					+ zaliczkaNaPod);
+			outPut.add("Kwota wolna od podatku = " + kwotaZmiejsz);
+			double podatekPotracony = zaliczkaNaPod - kwotaZmiejsz;
+			outPut.add("Podatek potrÄ…cony = "
+					+ df00.format(podatekPotracony));
+			obliczZaliczke();
+			zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
+			outPut.add("Zaliczka do urzÄ™du skarbowego = "
+					+ df00.format(zaliczkaUS) + " po zaokrÄ…gleniu = "
+					+ df.format(zaliczkaUS0));
+			double wynagrodzenie = podstawa
+					- ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
+			outPut.add("\n");
+			outPut.add("Pracownik otrzyma wynagrodzenie netto w wysokoĹ›ci = "
+							+ df00.format(wynagrodzenie));
+		} else if (umowa == 'Z') {
+			outPut.add("UMOWA-ZLECENIE");
+			outPut.add("Podstawa wymiaru skĹ‚adek " + podstawa);
+			double oPodstawa = obliczonaPodstawa(podstawa);
+			outPut.add("SkĹ‚adka na ubezpieczenie emerytalne "
+					+ df00.format(s_emerytalna));
+			outPut.add("SkĹ‚adka na ubezpieczenie rentowe    "
+					+ df00.format(s_rentowa));
+			outPut.add("SkĹ‚adka na ubezpieczenie chorobowe  "
+					+ df00.format(u_chorobowe));
+			outPut.add("Podstawa wymiaru skĹ‚adki na ubezpieczenie zdrowotne: "
+							+ oPodstawa);
+			obliczUbezpieczenia(oPodstawa);
+			outPut.add("SkĹ‚adka na ubezpieczenie zdrowotne: 9% = "
+					+ df00.format(s_zdrow1) + " 7,75% = " + df00.format(s_zdrow2));
+			kwotaZmiejsz = 0;
+			kosztyUzyskania = (oPodstawa * 20) / 100;
+			outPut.add("Koszty uzyskania przychodu (staĹ‚e) "
+					+ kosztyUzyskania);
+			double podstawaOpodat = oPodstawa - kosztyUzyskania;
+			double podstawaOpodat0 = Double.parseDouble(df.format(podstawaOpodat));
+			outPut.add("Podstawa opodatkowania " + podstawaOpodat
+					+ " zaokrÄ…glona " + df.format(podstawaOpodat0));
+			obliczPodatek(podstawaOpodat0);
+			outPut.add("Zaliczka na podatek dochodowy 18 % = "
+					+ zaliczkaNaPod);
+			double podatekPotracony = zaliczkaNaPod;
+			outPut.add("Podatek potrÄ…cony = "
+					+ df00.format(podatekPotracony));
+			obliczZaliczke();
+			zaliczkaUS0 = Double.parseDouble(df.format(zaliczkaUS));
+			outPut.add("Zaliczka do urzÄ™du skarbowego = "
+					+ df00.format(zaliczkaUS) + " po zaokrÄ…gleniu = "
+					+ df.format(zaliczkaUS0));
+			double wynagrodzenie = podstawa
+					- ((s_emerytalna + s_rentowa + u_chorobowe) + s_zdrow1 + zaliczkaUS0);
+			outPut.add("\n");
+			outPut.add("Pracownik otrzyma wynagrodzenie netto w wysokoĹ›ci = "
+							+ df00.format(wynagrodzenie));
+			
+		} else {
+			outPut.add("Nieznany typ umowy!");
+		}
+		reset();
+		return outPut;
+	}
+	
+	
 	public static void obliczZaliczke() {
 		zaliczkaUS = zaliczkaNaPod - s_zdrow2 - kwotaZmiejsz;
 	}
---
+++ example-taxes/test/org/tstraszewski/TaxCalculatorTest.java
@@ -0,0 +1,101 @@
+package org.tstraszewski;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.ws.BindingType;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.bartoszwalter.students.taxes.TaxCalculator;
+
+public class TaxCalculatorTest {
+
+	Map<String,List<String>> goodResults = new HashMap<String, List<String>>();
+	
+	@Before 
+	public void init(){
+		
+		double kwota = 1000;
+		goodResults.put(""+kwota+'P',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'P'));
+		kwota = 150000;
+		goodResults.put(""+kwota+'P',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'P'));
+		kwota = 100.50;
+		goodResults.put(""+kwota+'P',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'P'));
+		kwota = 0;
+		goodResults.put(""+kwota+'P',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'P'));
+		kwota = -100;
+		goodResults.put(""+kwota+'P',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'P'));
+		kwota = 1000;
+		goodResults.put(""+kwota+'Z',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Z'));
+		kwota = 134000;
+		goodResults.put(""+kwota+'Z',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Z'));
+		kwota = 1000.80;
+		goodResults.put(""+kwota+'Z',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Z'));
+		kwota = -100;
+		goodResults.put(""+kwota+'Z',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Z'));
+		kwota = -100;
+		goodResults.put(""+kwota+'P',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'P'));
+		kwota = 1000.80;
+		goodResults.put(""+kwota+'Y',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Y'));
+		kwota = 10.80;
+		goodResults.put(""+kwota+'Y',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Y'));
+		kwota = 100.20;
+		goodResults.put(""+kwota+'Y',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Y'));
+		kwota = -100;
+		goodResults.put(""+kwota+'Y',TaxCalculator.getArrayWithResultsUsingOriginalAlgorithm(kwota, 'Y'));
+	}
+	
+	@Test
+	public void taxTest(){
+		
+		paramterizedTest(1000, 'P');
+		paramterizedTest(150000, 'P');
+		paramterizedTest(100.50, 'P');
+		paramterizedTest(0, 'P');
+		paramterizedTest(-100, 'P');
+		paramterizedTest(1000, 'Z');
+		paramterizedTest(134000, 'Z');
+		paramterizedTest(1000.80, 'Z');
+		paramterizedTest(-100, 'Z');
+		paramterizedTest(-100, 'P');
+		paramterizedTest(1000.80, 'Y');
+		paramterizedTest(10.80, 'Y');
+		paramterizedTest(100.20, 'Y');
+		paramterizedTest(-100, 'Y');
+		
+		
+	}
+	
+	public void paramterizedTest(double kwota, char typ){
+		
+		String k = ""+kwota+typ;
+		List<String> goodRes = goodResults.get(k);
+		
+		List<String> myRes = TaxCalculator.getArrayWithResults(kwota, typ);
+		
+		if(goodRes.size() != myRes.size()){
+			Assert.assertEquals(true, false);
+		}
+		
+		int len = myRes.size();
+		if(goodRes != null){
+			
+			for(int i = 0 ; i < len; i++){
+				String good = goodRes.get(i);
+				String my = myRes.get(i);
+//				System.out.println("GOOD: " + good);
+//				System.out.println("MY: " + my);
+				Assert.assertTrue(good.equals(my));
+			}
+			
+		}else{
+			Assert.assertEquals(true, false);
+		}
+		
+	}
+}
